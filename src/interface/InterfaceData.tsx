03f3bc7915644af36f3e10192e5e8f71fa01240a
namvaau <hoainam.vaau@gmail.com>
2025-03-18 11:29:58 +0700

    Fix 18/3

--- src/interface/InterfaceData.tsx
+++ src/interface/InterfaceData.tsx
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:8080";
-// export const BASE_URL = "https://pkdkdonghieube.onrender.com";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://pkdkdonghieube.onrender.com";
 
 export interface Specialty {
     id: number;
